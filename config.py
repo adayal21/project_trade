97c2ae7dc118ec39bd4a70a248da4fafde2e0a01
Aman <12amandayal12@gmail.com>
2026-05-29 09:45:26 +0530

    2 startategy

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ COINS = [
     # Combined backtest 2022-2026 (4H, 0.2% commission):
     "DOGE/USDT",   # Combined +271% | HMA +133% | ICHI +138% | BEST
     "ADA/USDT",    # Combined +199% | HMA +152% | ICHI  +47%
-    "MATIC/USDT",  # Combined +166% | HMA  +86% | ICHI  +80%
+    "POL/USDT",    # Combined +166% | HMA  +86% | ICHI  +80% (MATIC rebranded to POL)
     "SOL/USDT",    # Combined +160% | HMA  +74% | ICHI  +86%
     "BNB/USDT",    # Combined  +89% | HMA  +21% | ICHI  +68%
     "XRP/USDT",    # Combined  +87% | HMA  +91% | ICHI   -4%
